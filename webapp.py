5c34115dff3ef2fd37705879501f5a71132fe016
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-01-12 09:15:12 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,13 +2,12 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET'])
 def render_main():
     return render_template('home.html')
-
 @app.route("/response", methods=['POST'])
 def render_response():
-    color = request.forms['color']
+    color = request.form['color']
     #The request object stores information about the request sent to the server. 
     #args is a MultiDict (like a dictionary bu tcan have multiple values for the same key)
     #The information in args is visible in the url for the page being requested (ex. .../response?color=blue
